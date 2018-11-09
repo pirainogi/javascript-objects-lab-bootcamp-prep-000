2dd6c3e9f65e6b9733e428bc1a0ba8602f4a0232
Gabbie Piraino <pirainogi@gmail.com>
2018-11-09 01:31:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object, key){
-  var newObj = Object.assign({} )
+  var newObj = Object.assign({}, object)
+  delete 
 }
 
 function destructivelyDeleteFromObjectByKey(object, key){
